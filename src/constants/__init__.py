3b2c001c52b8c04dd89fb8bfa57a8a30e1ff9136
Bore237 <goudjouborel@gmail.com>
2026-07-22 00:22:15 +0200

    refactor: change config file to config_cls to reflect more our work and update the constant file

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -2,6 +2,6 @@ import os
 from pathlib import Path
 
 # On remonte jusqu'à la racine du projet
-MEAN = 3724.6596
-STD = 8457.1860
-CONFIG_FILE_PATH = Path("src/config/config.yml")
+MEAN = 0.3479
+STD = 0.2546 
+CONFIG_FILE_PATH = Path(r"src\configs\classification\config_cls.yml") 
